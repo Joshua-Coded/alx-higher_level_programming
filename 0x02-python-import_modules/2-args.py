5185a1fab626222758aa84b201497cf591fbcfee
Joshbam_joshwilly <alanajoshua535@gmail.com>
2022-09-22 16:45:38 -0700

    error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-import sys
 
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
 
-def args():
-    if len(sys.argv) == 1:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif len(sys.argv) == 2:
+    elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
-if __name__ == '__main__':
-    args()
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
