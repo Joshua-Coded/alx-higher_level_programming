4d02985effd5bdadb0f6b556e30a1465b5f9ba6e
Joshbam_joshwilly <alanajoshua535@gmail.com>
2022-09-22 13:38:18 -0700

    task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 import sys
+
 
 def args():
     if len(sys.argv) == 1:
@@ -11,5 +11,5 @@ def args():
         print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
-if __name__ == "__main__":
+if __name__ == '__main__':
     args()
