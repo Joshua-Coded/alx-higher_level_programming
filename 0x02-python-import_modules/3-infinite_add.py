4246b0fe87f31f717baace8aa1efa3b0bf782fb1
Joshbam_joshwilly <alanajoshua535@gmail.com>
2022-09-22 16:47:46 -0700

    error fixed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-import sys
 
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
 
-def infinite_add():
-    sum = 0
-    for i in range(1, len(sys.argv)):
-        sum += int(sys.argv[i])
-    print("{}".format(sum))
-if __name__ == '__main__':
-    infinite_add()
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
