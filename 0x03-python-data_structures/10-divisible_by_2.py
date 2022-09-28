076c347778c679b93b7d99e7dd9c3321c0ae0e6f
Joshbam_joshwilly <alanajoshua535@gmail.com>
2022-09-27 21:46:37 -0700

    files

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,11 @@
-
-
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
+    """Finds all multiples of 2 in a list"""
+    multiples = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            new_list.append(True)
+            multiples.append(True)
         else:
-            new_list.append(False)
-    return new_list
+            multiples.append(False)
+
+    return multiples
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    my_tuple = ()
-    if len(sentence) == 0:
-        my_tuple = 0, "None"
-    else:
-        my_tuple = len(sentence), sentence[0]
-    return my_tuple
+    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
