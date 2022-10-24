106908f33fd2294009b7c1506dd907f94cc7af30
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-10-24 05:16:32 -0700

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,18 +1,5 @@
-class Object :
-    one = "first"
-    two = "second"
-    three = "third"
+#!/usr/bin/python3
 
 
-    def __init__(self, attr):
-        self.attr = attr
-
-    def lookup(self):
-        print(self.one, self.two, self.three, self.attr)
-
-n = Object(2)
-n.lookup()
-
-
-# passing both the objects
-# and the class as argument
+def lookup(obj):
+    return dir(obj)
