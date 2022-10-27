19ad08e9f53ef531b7e070e8a7483ef0f932c541
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-10-26 18:10:10 -0700

    tasks

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 
-"""saving a json file function"""
+"""saving into json file"""
+
 import json
 
 def save_to_json_file(my_obj, filename):
 
-    """the function"""
-
+    """function for saving using dump"""
     with open("filename", "w") as f:
         json.dump(my_obj, f)
